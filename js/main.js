40e0414cbf5777f32fa4450027c1ba03c8ecde39
Victor Kalinkin <vakalinkin@hotmail.com>
2021-06-08 15:09:09 -0700

    image, year, type, episode count of show populate list in html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var $resultsList = document.querySelector('.resultsList');
 
-var searchTem = 'gundam';
+var searchTem = 'street fighter';
 
 var xhrTest = new XMLHttpRequest();
 xhrTest.open('GET', 'https://lfz-cors.herokuapp.com/?url=https://api.jikan.moe/v3/search/anime?q=' + searchTem);
@@ -15,10 +15,31 @@ xhrTest.addEventListener('load', function () {
     seriesObj = searchResults[a];
     var $series50 = document.createElement('div');
     $series50.className = 'div series50';
-    var titleDiv = document.createElement('div');
+
     // console.log(seriesObj);
+
+    var showImage = document.createElement('img');
+    showImage.setAttribute('src', seriesObj.image_url);
+    $series50.appendChild(showImage);
+
+    var titleDiv = document.createElement('div');
     titleDiv.textContent = seriesObj.title;
     $series50.appendChild(titleDiv);
+
+    var typeDiv = document.createElement('div');
+    typeDiv.textContent = seriesObj.type;
+    $series50.appendChild(typeDiv);
+
+    var yearDiv = document.createElement('div');
+    var yearString = String(seriesObj.start_date);
+    yearString = yearString.slice(0, 4);
+    yearDiv.textContent = yearString;
+    $series50.appendChild(yearDiv);
+
+    var episodesDiv = document.createElement('div');
+    episodesDiv.textContent = 'Episodes: ' + seriesObj.episodes;
+    $series50.appendChild(episodesDiv);
+
     $resultsList.appendChild($series50);
 
   }
