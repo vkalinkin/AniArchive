52983b427548fb98467411701c11d4836dec7dc9
Victor Kalinkin <vakalinkin@hotmail.com>
2021-06-11 10:52:38 -0700

    finished styling for modal, works on mobile and desktop

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@ function xhrReqAnime() {
       $series50.appendChild(yearDiv);
 
       var episodesDiv = document.createElement('div');
-      episodesDiv.textContent = 'Episodes: ' + seriesObj.episodes;
+      episodesDiv.textContent = 'Episode(s): ' + seriesObj.episodes;
       $series50.appendChild(episodesDiv);
 
       var butDiv = document.createElement('div');
@@ -111,7 +111,7 @@ function xhrReqManga() {
       $series50.appendChild(yearDiv);
 
       var chaptersDiv = document.createElement('div');
-      chaptersDiv.textContent = 'Chapters: ' + seriesObj.chapters;
+      chaptersDiv.textContent = 'Chapter(s): ' + seriesObj.chapters;
       $series50.appendChild(chaptersDiv);
 
       var butDiv = document.createElement('div');
@@ -190,9 +190,9 @@ function xhrReqIDAnime(id) {
   xhr.setRequestHeader('token', 'abc123');
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
-    // console.log(xhr.response);
     var currentAnime = xhr.response;
-    // console.log('currentAnime', currentAnime);
+
+    var modalData = document.createElement('div');
 
     var modalTopDiv = document.createElement('div');
     modalTopDiv.className = 'modalTop row';
@@ -202,77 +202,102 @@ function xhrReqIDAnime(id) {
 
     var modalImg = document.createElement('img');
     modalImg.setAttribute('src', currentAnime.image_url);
+    modalImg.className = 'infoImg';
     imgSpan.appendChild(modalImg);
 
     var textSpan = document.createElement('span');
+    textSpan.className = 'textSpan';
     modalTopDiv.appendChild(textSpan);
 
     var titleDiv = document.createElement('div');
     titleDiv.textContent = currentAnime.title;
+    titleDiv.className = 'infoRow';
     textSpan.appendChild(titleDiv);
 
     var titleEngDiv = document.createElement('div');
     titleEngDiv.textContent = currentAnime.title_english;
+    titleEngDiv.className = 'infoRow';
     textSpan.appendChild(titleEngDiv);
 
     var yearDiv = document.createElement('div');
     var airTimes = currentAnime.aired.from;
     var year = airTimes.slice(0, 4);
     yearDiv.textContent = year;
+    yearDiv.className = 'infoRow';
     textSpan.appendChild(yearDiv);
 
     var typeDiv = document.createElement('div');
     typeDiv.textContent = currentAnime.type;
+    typeDiv.className = 'infoRow';
     textSpan.appendChild(typeDiv);
 
     var episodesDiv = document.createElement('div');
-    episodesDiv.textContent = 'Episodes: ' + currentAnime.episodes;
+    episodesDiv.textContent = 'Episode(s): ' + currentAnime.episodes;
+    episodesDiv.className = 'infoRow';
     textSpan.appendChild(episodesDiv);
 
     var durationDiv = document.createElement('div');
     durationDiv.textContent = currentAnime.duration;
+    durationDiv.className = 'infoRow';
     textSpan.appendChild(durationDiv);
 
     var sourceDiv = document.createElement('div');
     sourceDiv.textContent = 'Source: ' + currentAnime.source;
+    sourceDiv.className = 'infoRow';
     textSpan.appendChild(sourceDiv);
 
     var ratingDiv = document.createElement('div');
-    ratingDiv.textContent = currentAnime.rating;
+    ratingDiv.textContent = 'Rated ' + currentAnime.rating;
+    ratingDiv.className = 'infoRow';
     textSpan.appendChild(ratingDiv);
 
     var studiosDiv = document.createElement('div');
     var currentStudios = currentAnime.studios;
+    studiosDiv.className = 'infoRow';
     studiosDiv.textContent = 'Studio(s): ';
     for (var b = 0; b < currentStudios.length; b++) {
       var curStudio = currentStudios[b];
-      studiosDiv.textContent += curStudio.name + ' ';
+      if (b === currentStudios.length - 1) {
+        studiosDiv.textContent += curStudio.name;
+      } else {
+        studiosDiv.textContent += curStudio.name + ', ';
+      }
+
     }
     textSpan.appendChild(studiosDiv);
 
-    $modalContent.appendChild(modalTopDiv);
+    modalData.appendChild(modalTopDiv);
 
     var genresDiv = document.createElement('div');
     var currentGenres = currentAnime.genres;
+    genresDiv.className = 'infoRow';
     genresDiv.textContent = 'Genre(s): ';
     for (var a = 0; a < currentGenres.length; a++) {
       var curGenre = currentGenres[a];
-      genresDiv.textContent += curGenre.name + ' ';
+      if (a === currentGenres.length - 1) {
+        genresDiv.textContent += curGenre.name;
+      } else {
+        genresDiv.textContent += curGenre.name + ', ';
+      }
     }
-    $modalContent.appendChild(genresDiv);
+    modalData.appendChild(genresDiv);
 
     var synopDiv = document.createElement('div');
     synopDiv.textContent = 'Synopsis: ' + currentAnime.synopsis;
-    $modalContent.appendChild(synopDiv);
+    synopDiv.className = 'infoRow synop';
+    modalData.appendChild(synopDiv);
 
     var modalBut = document.createElement('div');
+    modalBut.className = 'modalBut';
 
     var modalOkButtonSpan = document.createElement('span');
     modalOkButtonSpan.textContent = 'OK';
     modalOkButtonSpan.className = 'modalOkButton';
     modalBut.appendChild(modalOkButtonSpan);
 
-    $modalContent.appendChild(modalBut);
+    modalData.appendChild(modalBut);
+
+    $modalContent.appendChild(modalData);
   });
   xhr.send();
 }
@@ -295,70 +320,61 @@ function xhrReqIDManga(id) {
 
     var modalImg = document.createElement('img');
     modalImg.setAttribute('src', currentManga.image_url);
+    modalImg.className = 'infoImg';
     imgSpan.appendChild(modalImg);
 
     var textSpan = document.createElement('span');
+    textSpan.className = 'textSpan';
     modalTopDiv.appendChild(textSpan);
 
     var titleDiv = document.createElement('div');
     titleDiv.textContent = currentManga.title;
+    titleDiv.className = 'infoRow';
     textSpan.appendChild(titleDiv);
 
     var titleEngDiv = document.createElement('div');
     titleEngDiv.textContent = currentManga.title_english;
+    titleEngDiv.className = 'infoRow';
     textSpan.appendChild(titleEngDiv);
 
     var yearDiv = document.createElement('div');
+    yearDiv.className = 'infoRow';
     var pub = currentManga.published;
-    // console.log('pub', pub);
     var fromPub = pub.from;
-    // console.log('fromPub:', fromPub);
     var year = fromPub.slice(0, 4);
     yearDiv.textContent = year;
     textSpan.appendChild(yearDiv);
 
     var typeDiv = document.createElement('div');
     typeDiv.textContent = currentManga.type;
+    typeDiv.className = 'infoRow';
     textSpan.appendChild(typeDiv);
 
     var chaptersDiv = document.createElement('div');
-    chaptersDiv.textContent = 'Chapters: ' + currentManga.chapters;
+    chaptersDiv.textContent = 'Chapter(s): ' + currentManga.chapters;
+    chaptersDiv.className = 'infoRow';
     textSpan.appendChild(chaptersDiv);
 
-    // var durationDiv = document.createElement('div');
-    // durationDiv.textContent = currentAnime.duration;
-    // textSpan.appendChild(durationDiv);
-
-    // var sourceDiv = document.createElement('div');
-    // sourceDiv.textContent = 'Source: ' + currentAnime.source;
-    // textSpan.appendChild(sourceDiv);
-
-    // var ratingDiv = document.createElement('div');
-    // ratingDiv.textContent = currentAnime.rating;
-    // textSpan.appendChild(ratingDiv);
-
-    // var studiosDiv = document.createElement('div');
-    // var currentStudios = currentAnime.studios;
-    // studiosDiv.textContent = 'Studio(s): ';
-    // for (var b = 0; b < currentStudios.length; b++) {
-    //   var curStudio = currentStudios[b];
-    //   studiosDiv.textContent += curStudio.name + ' ';
-    // }
-    // textSpan.appendChild(studiosDiv);
     var authorDiv = document.createElement('div');
+    authorDiv.className = 'infoRow';
     var currentAuthors = currentManga.authors;
-    // console.log('currentAuthors:', currentAuthors);
+
     authorDiv.textContent = 'Author(s): ';
     for (var b = 0; b < currentAuthors.length; b++) {
-      // console.log('curAuthor:', curAuthor);
       var curAuthor = currentAuthors[b];
       var author = curAuthor.name;
-      authorDiv.textContent += author + ' ';
+      if (b === currentAuthors.length - 1) {
+        authorDiv.textContent += author;
+      } else {
+        authorDiv.textContent += author + ' | ';
+      }
+
     }
     textSpan.appendChild(authorDiv);
 
     var serialDiv = document.createElement('div');
     var currentSerial = currentManga.serializations;
+    serialDiv.className = 'infoRow';
     serialDiv.textContent = 'Serialization(s): ';
     for (var c = 0; c < currentSerial.length; c++) {
       var serial = currentSerial[c].name;
@@ -369,19 +385,26 @@ function xhrReqIDManga(id) {
     $modalContent.appendChild(modalTopDiv);
 
     var genresDiv = document.createElement('div');
+    genresDiv.className = 'infoRow';
     var currentGenres = currentManga.genres;
     genresDiv.textContent = 'Genre(s): ';
     for (var a = 0; a < currentGenres.length; a++) {
       var curGenre = currentGenres[a];
-      genresDiv.textContent += curGenre.name + ' ';
+      if (a === currentGenres.length - 1) {
+        genresDiv.textContent += curGenre.name;
+      } else {
+        genresDiv.textContent += curGenre.name + ', ';
+      }
     }
     $modalContent.appendChild(genresDiv);
 
     var synopDiv = document.createElement('div');
     synopDiv.textContent = 'Synopsis: ' + currentManga.synopsis;
+    synopDiv.className = 'infoRow synop';
     $modalContent.appendChild(synopDiv);
 
     var modalBut = document.createElement('div');
+    modalBut.className = 'modalBut';
 
     var modalOkButtonSpan = document.createElement('span');
     modalOkButtonSpan.textContent = 'OK';
@@ -426,23 +449,19 @@ $seasonSearch.addEventListener('submit', function (event) {
   }
 
   var year = season.slice(0, 4);
-  // console.log('year:', year);
   var seas = season.slice(4);
-  // console.log('season:', seas);
   xhrReqSeason(year, seas);
 });
 
 $searchSelect.addEventListener('click', function (event) {
   event.preventDefault();
   if (event.target === $termButton) {
-    // console.log('term button clicked!');
     $termSearch.className = 'row termSearch';
     $seasonSearch.className = 'row seasonSearch hidden';
     $termButton.className = 'termButton active';
     $seasonButton.className = 'seasonButton inactive';
 
   } else if (event.target === $seasonButton) {
-    // console.log('season button clicked!');
     $termSearch.className = 'row termSearch hidden';
     $seasonSearch.className = 'row seasonSearch';
     $termButton.className = 'termButton inactive';
@@ -451,23 +470,12 @@ $searchSelect.addEventListener('click', function (event) {
 });
 
 $resultsList.addEventListener('click', function (event) {
-  // console.log('clicked!');
-  // console.log('event taget:', event.target);
-  // console.log('$moreInfoButtonSpan', $moreInfoButtonSpan);
-  // if (event.target === $moreInfoButtonSpan){
-  //   console.log('more info button pressed!');
-  // }
-  // if (event.target === $butDiv){
-  //   console.log('butDiv pressed!');
-  // }
   if (event.target.className === 'moreInfoButton') {
-    // console.log('CLICKED!')
-    // console.log('target with class name of more Info button clicked!');
+
     $modalContent.replaceChildren();
     $modal.className = 'modal';
     var currentMedium = event.target.getAttribute('medium');
     var currentId = event.target.getAttribute('id');
-    // console.log('current id:', currentId);
     if (currentMedium === 'anime') {
       xhrReqIDAnime(currentId);
     } else if (currentMedium === 'manga') {
@@ -479,7 +487,6 @@ $resultsList.addEventListener('click', function (event) {
 
 $modalContent.addEventListener('click', function (event) {
   if (event.target.className === 'modalOkButton') {
-    // console.log('modal ok button pressed!');
     $modal.className = 'modal hidden';
   }
 });
